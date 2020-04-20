b79056726a54a11e8b9df5caa5088984eb10d30f
leaf.shi <leaf.shi@gmail.com>
2020-04-20 10:25:36 +0800

    三数之和-优化去重并增加总结文章

--- Week_01/threeSum/Solution.java
+++ Week_01/threeSum/Solution.java
@@ -1,44 +1,35 @@
+// https://leetcode-cn.com/problems/3sum/
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.LinkedHashSet;
 
 class Solution {
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
-        int fi = -100, fj = -100, fk = -100;
         for (int i = 0; i < nums.length; i++) {
-            for(int j = i + 1, k = nums.length - 1; j < k; ) {
-                if (-1 * nums[i] < nums[j] + nums[k]) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1, k = nums.length - 1;
+            while ( j < k ) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> current = new ArrayList<Integer>();
+                    current.add(nums[i]);
+                    current.add(nums[j]);
+                    current.add(nums[k]);
+                    result.add(current);
+                    j++;
                     k--;
-                } else if (-1 * nums[i] > nums[j] + nums[k]) {
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                    while (k > j && nums[k] == nums[k+1]) k--;
+                } else if (sum < 0) {
                     j++;
                 } else {
-                    if (result.size() > 0 && fi == nums[i] && fj == nums[j] && fk == nums[k]) {
-                        //duplicate
-                    } else {
-                        fi = nums[i];
-                        fj = nums[j];
-                        fk = nums[k];
-
-                        List<Integer> tmp = new ArrayList<Integer>();
-                        tmp.add(nums[i]);
-                        tmp.add(nums[j]);
-                        tmp.add(nums[k]);
-                        result.add(tmp);
-                    }
-                    j++;
                     k--;
                 }
             }
         }
-
-        LinkedHashSet<List<Integer>> set = new LinkedHashSet<List<Integer>>(result.size());
-        set.addAll(result);
-        result.clear();
-        result.addAll(set);
         return result;
     }
 
