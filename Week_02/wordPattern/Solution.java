3aedaa52f80ac9ff4a6fc6fd0c386ae0aa1dbd0b
leaf.shi <leaf.shi@gmail.com>
2020-04-24 09:33:37 +0800

    单词规律-代码+文档+进度

--- Week_02/wordPattern/Solution.java
+++ Week_02/wordPattern/Solution.java
@@ -1,9 +1,7 @@
-# 单词规律
-[LeetCode地址](https://leetcode-cn.com/problems/word-pattern)
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-## 第一次
-一开始没看懂，不得不佩服。第一层：map的返回值；第二层：Objects.equals()
-```java
 class Solution {
     public boolean wordPattern(String pattern, String str) {
         String[] word = str.split(" ");
@@ -14,11 +12,20 @@ class Solution {
         for (int i = 0; i < word.length; i++) {
             Integer pi = map.put(pattern.charAt(i), i);
             Integer si = map.put(word[i], i);
+            System.out.println("-----");
+            System.out.println(pi);
+            System.out.println(si);
             if (!Objects.equals(pi, si)) {
                 return false;
             }
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String pattern = "abba";
+        String str = "dog cat cat dog";
+        System.out.println(solution.wordPattern(pattern, str));
+    }
 }
-```
