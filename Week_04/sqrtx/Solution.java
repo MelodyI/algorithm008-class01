8e4c9a1e6f1e1e31aa79fd84b5ef9b65c3460886
leaf.shi <leaf.shi@gmail.com>
2020-04-30 13:46:09 +0800

    x的平方根-代码+文档+进度

---
+++ Week_04/sqrtx/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        double x0 = x;
+        double x1 = (x0 + x/x0) / 2;
+        while (Math.abs(x0 - x1) >= 1) {
+            x0 = x1;
+            x1 = (x0 + x/x0) / 2;
+        }
+        return (int)x1;
+    }
+}
