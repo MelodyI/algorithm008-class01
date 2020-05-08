a8e67e48e5a1636a2f4ab1fdafc2786a012c1e52
leaf.shi <leaf.shi@gmail.com>
2020-05-08 15:54:50 +0800

    二叉树的层序遍历-修正函数名称

--- Week_04/binaryTreeLevelOrderTraversal/Solution.java
+++ Week_04/binaryTreeLevelOrderTraversal/Solution.java
@@ -4,11 +4,11 @@ class Solution {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         ans = new ArrayList<>();
-        _dsf(root, 0);
+        _dfs(root, 0);
         return ans;
     }
 
-    private void _dsf(TreeNode node, int level) {
+    private void _dfs(TreeNode node, int level) {
         // terminator
         if (node == null) return;
         // process current logic
@@ -16,8 +16,8 @@ class Solution {
             ans.add(new ArrayList<Integer>());
         ans.get(level).add(node.val);
         // drill down
-        _dsf(node.left, level + 1);
-        _dsf(node.right, level + 1);
+        _dfs(node.left, level + 1);
+        _dfs(node.right, level + 1);
         // reverse state
     }
 }
